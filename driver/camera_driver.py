fb5599c709bd659bf214a71730585f9dd5af9246
ritsuki-takesako <ritsuki.takesako21@imperial.ac.uk>
2024-07-16 13:37:39 +0200

    driver for camera (taking image)

---
+++ driver/camera_driver.py
@@ -0,0 +1,42 @@
+import time
+import os
+import serial
+import cv2 as cv
+
+class camera():
+
+    def __init__(self,conf):  
+        self.port = conf['port']
+        self.width = conf['width']
+        self.height = conf['height']
+        self.exposure = conf['exposure']
+        self.manual_exp = conf['manual_exp']
+         
+
+    def take_image(self, composition_1:str, composition_2:str, composition_3:str, composition_1_qua:int, composition_2_qua:int, composition_3_qua:int):
+        documents_path = os.path.expanduser('~\Documents')
+        IMAGE_DIR = os.path.join(documents_path, composition_1+"_"+composition_2+"_"+composition_3+"_"+'titration_image')
+        cap = cv.VideoCapture(self.port,cv.CAP_DSHOW)
+        if self.manual_exp==True:
+            manual_exp=0.25
+        else:
+            manual_exp=0.75
+        cap.set(cv.CAP_PROP_FRAME_WIDTH, self.width)
+        cap.set(cv.CAP_PROP_FRAME_HEIGHT, self.height)
+        cap.set(cv.CAP_PROP_AUTO_EXPOSURE, manual_exp)  # Manual exposure
+        cap.set(cv.CAP_PROP_EXPOSURE, self.exposure)  # Adjust exposure value
+        if cap.isOpened():
+            ret, frame = cap.read() # Try to get the first frame
+        else:
+            print("Cannot open camera")
+            exit()
+        ret, frame = cap.read()
+        print("image_taken")
+        #time_stamp = time.strftime("%Y_%m_%d_%Hh_%Mm_%Ss", time.localtime())
+        dir_name = os.path.join(IMAGE_DIR, str(composition_1_qua)+"_"+str(composition_2_qua)+"_"+str(composition_3_qua)+".jpg")
+        if not os.path.exists(IMAGE_DIR):
+            os.makedirs(IMAGE_DIR)
+        cv.imwrite(dir_name, frame)
+        print(f"Image has been saved: {dir_name}")
+        cap.release()
+        return dir_name
