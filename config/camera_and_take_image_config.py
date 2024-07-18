54fe8edbcf760a306ed21b7e9f414c1225fef758
ritsuki-takesako <ritsuki.takesako21@imperial.ac.uk>
2024-07-18 12:25:22 +0200

    Update camera_and_take_image_config.py

--- config/camera_and_take_image_config.py
+++ config/camera_and_take_image_config.py
@@ -10,7 +10,7 @@ config['servers'] = dict(orchestrator = dict(host="127.0.0.1",port=13380),
                          image = dict(host="127.0.0.1",port=13388))
 
 #config information for action and driver servers
-config['psdDriver'] = dict(port=6, baud=9600, psd_type = '4', psd_syringe = '1.25m', speed = 10) #PSD.PSDTypes.psd4.value, PSD.SyringeTypes.syringe125mL.value
+config['psdDriver'] = dict(port=4, baud=9600, psd_type = '4', psd_syringe = '1.25m', speed = 10) #PSD.PSDTypes.psd4.value, PSD.SyringeTypes.syringe125mL.value
 config['psd'] = dict(url="http://127.0.0.1:13370", valve = {'S1': 2, 'S2': 3, 'S3': 4, 'S4': 5, 'S5': 6, 'S6': 7, 'Out': 1, 'Mix': 8}, volume = 1250, speed = 10)
 config['cameraDriver'] = dict(port=1, width = 640, height = 480, exposure = -1, manual_exp = True)
 config['camera'] = dict(url="http://127.0.0.1:13385")
@@ -21,8 +21,8 @@ config['image'] = dict(url="http://127.0.0.1:13387")
 config['orchestrator'] = dict(path=r'C:\Users\Operator\Documents\data',kadiurl=None)
 
 #
-config['launch'] = dict(server = ['cameraDriver','imageDriver'],
-                        action = ['camera','image'],
+config['launch'] = dict(server = ['psdDriver','cameraDriver','imageDriver'],
+                        action = ['psd','camera','image'],
                         orchestrator = ['orchestrator']
                         )
 
