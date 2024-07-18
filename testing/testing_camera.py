a377518333b978b70a74707ba57bb57a560ffe0c
ritsuki-takesako <ritsuki.takesako21@imperial.ac.uk>
2024-07-18 09:38:38 +0200

    Update testing_camera.py

--- testing/testing_camera.py
+++ testing/testing_camera.py
@@ -5,6 +5,7 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import json
 sys.path.append(r'../config')
 sys.path.append(r'../action')
 sys.path.append(r'../server')
@@ -12,6 +13,15 @@ import time
 #from config.mischbares_small import config
 from config.camera_and_take_image_config import config
 
+
+def call_orchestrator (sequence,thread=0):
+    server = 'orchestrator'
+    action = 'addExperiment'
+    params = dict(experiment=json.dumps(sequence),thread=thread)
+    print("requesting")
+    requests.post("http://{}:{}/{}/{}".format(
+        config['servers']['orchestrator']['host'], 13380, server, action), params=params).json()
+    
 def take_image(action, params):
     server = 'camera'
     action = action
@@ -35,9 +45,13 @@ def image_analysis(action, params):
     return res
 
 #taking an image
-image_path = take_image('take_image', params=dict(composition_1 = "ethanoic_acid", composition_2 = "phosphoric_acid", composition_3 = "sodium_hydroxide",composition_1_qua = 150, composition_2_qua = 100, composition_3_qua = 100))
-print (image_path)
-average_col = image_analysis('extract_color_from_roi', params=dict(image_path))
-print (average_col)
+call_orchestrator(dict(soe=['orchestrator/start','camera/take_image', 'image/extract_color_from_roi'],
+                       params={'start': {'collectionkey' : 'al_sequential'},
+                               'take_image':{'composition_1': "ethanoic_acid", 'composition_2':"phosphoric_acid", 'composition_3':"sodium_hydroxide", "composition_1_qua":150, "composition_2_qua":100, "composition_3_qua":100},
+                               'extract_color_from_roi':{'image_path':image_path}}))
+#image_path = take_image('take_image', params=dict(composition_1 = "ethanoic_acid", composition_2 = "phosphoric_acid", composition_3 = "sodium_hydroxide",composition_1_qua = 150, composition_2_qua = 100, composition_3_qua = 100))
+#print (image_path)
+#average_col = image_analysis('extract_color_from_roi', params=dict(image_path))
+#print (average_col)
 
                     
