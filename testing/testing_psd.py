f8ba3c6d2c5b2eee762787a3a7a12ec112f3876f
ritsuki-takesako <ritsuki.takesako21@imperial.ac.uk>
2024-07-19 09:47:46 +0200

    Update testing_psd.py

--- testing/testing_psd.py
+++ testing/testing_psd.py
@@ -21,7 +21,7 @@ def psd_test(action, params):
 # Aspirate from the single vial to the syringe and dispense to the cell
 ## Doesnt dispense to the cell!
 
-#psd_test('pumpMix', params=dict(V1 = 400, V2 = 0, V3 = 0, V4 = 0, V5 = 0, V6 = 0, speed = 10, mix = 1, times= 1, cell = True)) 
+psd_test('pumpMix', params=dict(V1 = 400, V2 = 0, V3 = 0, V4 = 0, V5 = 0, V6 = 0, speed = 10, mix = 1, times= 1, cell = True)) 
 # Aspirate from all desired vials to syringe, 
 # if mix =! 0, then liquid will be pumped between syringe and mix vial for mix times 
 # if cell = True, then dispense to the cell, else dispense to the mix vial
