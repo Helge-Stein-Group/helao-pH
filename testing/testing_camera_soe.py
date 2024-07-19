eaf95ae2dbf5ade5f63dad8b5c106c4626100687
ritsuki-takesako <ritsuki.takesako21@imperial.ac.uk>
2024-07-19 09:44:14 +0200

    Update testing_camera_soe.py

--- testing/testing_camera_soe.py
+++ testing/testing_camera_soe.py
@@ -54,11 +54,32 @@ def image_analysis(action, params):
     return res
 
 
-params_exp={'start': {'collectionkey' : 'camera_test_soe'},
-            'pumpMix_0': dict(V1 = 400, V2 = 0, V3 = 0, V4 = 0, V5 = 0, V6 = 0, speed = 10, mix = 1, times= 1, cell = False),
-            'takeImage_0': dict(composition_1 = "ethanoic_acid", composition_2 = "phosphoric_acid", composition_3 = "sodium_hydroxide",composition_1_qua = 150, composition_2_qua = 100, composition_3_qua = 100),
-            'extractColorFromRoi':{'image_path':r'C:\Users\DigiCat\Documents\1_1_1_titration_image'}}
+Total_vol = 1000
+x = y = np.linspace(0,1,6)
+X,Y = np.meshgrid(x,y)
+xy = np.vstack([X.ravel(),Y.ravel()]).T
+z = np.array([1-np.sum(xyi) for xyi in xy]).reshape(-1,1)
+xyz = np.hstack([xy,z])
+xyz = xyz[np.where(z>=0)[0],:]
+xyz = xyz*Total_vol
 
-test_fnc(dict(soe=['orchestrator/start','psd/pumpMix_0','camera/takeImage_0','image/extractColorFromRoi'], params=params_exp, meta=dict()))
+comp_1 = xyz[:,0]
+comp_2 = xyz[:,1]
+comp_3 = xyz[:,2]
+
+
+
+
+test_fnc(dict(soe=['orchestrator/start'], params = {'start': {'collectionkey' : 'camera_test_soe'}}))
+#for i in range(len(comp_1)):
+    #params_exp={'pumpMix_{i}': dict(V1 = comp_1[i], V2 = comp_2[i], V3 = comp_3[i], V4 = 0, V5 = 0, V6 = 0, speed = 10, mix = 1, times= 1, cell = False),
+                # 'takeImage_{i}': dict(composition_1 = "ethanoic_acid", composition_2 = "phosphoric_acid", composition_3 = "sodium_hydroxide",composition_1_qua = comp_1[i], composition_2_qua = comp_2[i], composition_3_qua = comp_3[i]),
+            #'extractColorFromRoi_{i}':{'image_path':r'C:\Users\DigiCat\Documents\1_1_1_titration_image'}}
+    #test_fnc(dict(soe=['psd/pumpMix_{i}','camera/takeImage_{i}','image/extractColorFromRoi_{i}'], params=params_exp, meta=dict()))
+
+for i in range(len(comp_1)):
+    params_exp={'pumpMix_{i}': dict(V1 = comp_1[i], V2 = comp_2[i], V3 = comp_3[i], V4 = 0, V5 = 0, V6 = 0, speed = 10, mix = 1, times= 1, cell = False),
+            'takeImage_{i}': dict(composition_1 = "ethanoic_acid", composition_2 = "phosphoric_acid", composition_3 = "sodium_hydroxide",composition_1_qua = comp_1[i], composition_2_qua = comp_2[i], composition_3_qua = comp_3[i])}
+    test_fnc(dict(soe=['psd/pumpMix_{i}','camera/takeImage_{i}'], params=params_exp, meta=dict()))
 
 test_fnc(dict(soe=['orchestrator/finish'], params={'finish': None}, meta={}))
